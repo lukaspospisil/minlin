05bce17d3b7f56220593e914032a2fa2f78e8981
lukaspospisil <lukas.pospisil@vsb.cz>
2016-01-11 10:15:00 +0100

    first version of qpopt can be compiled

--- examples_qpopt/string.cu
+++ examples_qpopt/string.cu
@@ -10,11 +10,13 @@ Solution of string problem with QPOPT library
 
 #include <minlin/minlin.h>
 #include <minlin/modules/threx/threx.h>
-#include <qpopt.h>
+#include <qpopt/qpopt.h>
 
 #include "savevtk.h"
 
 using namespace minlin::threx;
+
+MINLIN_INIT
 
 int main(int argc, char *argv[]) {
 
