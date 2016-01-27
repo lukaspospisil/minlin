d4be476ea3624443d8eabd832d68027e20a69343
lukaspospisil <lukas.pospisil@vsb.cz>
2016-01-27 11:35:37 +0100

    memory test ready to run on piz daint

--- tests/test_memory.cu
+++ tests/test_memory.cu
@@ -24,15 +24,11 @@ using namespace minlin::threx;
 #ifdef USE_GPU
 	/* compute using CUDA on Device */
 
-	#define MyVector DeviceVector
-
-	#define TEST_CPU false
+	#define TEST_CPU true
 	#define TEST_GPU true
 
 #else
 	/* compute without CUDA on Host */
-
-	#define MyVector HostVector
 
 	#define TEST_CPU true
 	#define TEST_GPU false
@@ -76,157 +72,50 @@ int main ( int argc, char *argv[] ) {
 
 	double t_start, t; /* to measure time */
 
-	/* fill vector with some values */
-	t_start = getUnixTime();
-
-
-	MyVector<Scalar> x(N);
-	x(all) = 0.0;
-	#ifdef USE_GPU
-		/* fill vector using CUDA */
-		// TODO: optimal number of threads/block
-		Scalar *xp = x.pointer();
-		fill_x<<<N, 1>>>(xp,N);
-		gpuErrchk( cudaDeviceSynchronize() );
-		
-	#else
-		/* fill vector using OpenMP */
-		#pragma omp parallel for private(k)
-		for(k=0;k<N;k++){
-			/* vector */
-			x(k) = 1.0 + 1.0/(Scalar)(k+1);
-		}	
-		
-	#endif
-		
-	std::cout << "init & fill vector: " << getUnixTime() - t_start << "s" << std::endl;
-
-
-	/* if it is MINLIN_FULL test, create&fill the matrix */
-	#if TEST_MINLIN_FULL
-		t_start = getUnixTime();
-
-		MyMatrix<Scalar> A(N,N);
-
-		A(all) = 0.0;
-
-		for(k=0;k<N;k++){
-			/* first row */
-			if(k == 0){
-				A(k,k) = 1.0;
-				A(k,k+1) = -1.0;
-			}
-			/* common row */
-			if(k > 0 && k < N-1){
-				A(k,k+1) = -1.0;
-				A(k,k) = 2.0;
-				A(k,k-1) = -1.0;
-			}
-			/* last row */
-			if(k == N-1){
-				A(k,k-1) = -1.0;
-				A(k,k) = 1.0;
-			}
-		}	
-		std::cout << "init & fill matrix: " << getUnixTime() - t_start << "s" << std::endl;
-
-	#endif
-
-	std::cout << std::endl;
 
 	/* to compute average time of each algorithm */
 	/* these variables store the sum of computing times */
-	#if TEST_MINLIN_FULL
-		double t_minlin_full = 0.0;
+	#if TEST_CPU
+		double t_cpu = 0.0;
+		HostVector<Scalar> x_cpu;
 	#endif
-	#if TEST_MINLIN
-		double t_minlin = 0.0;
-	#endif
-	#if TEST_FOR
-		double t_for = 0.0;
-	#endif
-	#if TEST_OMP
-		double t_omp = 0.0;
-	#endif
-	#if TEST_CUDA
-		double t_cuda = 0.0;
+	#if TEST_GPU
+		double t_gpu = 0.0;
+		DeviceVector<Scalar> x_gpu;
 	#endif
 
-	/* multiplication test */
-	MyVector<Scalar> Ax(N);
-	
-	/* I want to see the problems with setting the vector values immediately in the norm */
-	/* if I forget to set a component of Ax, then the norm will be huge */
-	Scalar default_value = std::numeric_limits<Scalar>::max(); 
 
 	for(k = 0;k < M;k++){ /* for every test */
 		std::cout << k+1 << ". test (of " << M << ")" << std::endl;
 		
-		#if TEST_MINLIN_FULL
-			Ax(all) = default_value; /* clean previous results */
+		#if TEST_CPU
 
 			t_start = getUnixTime();
-			my_multiplication_minlin_full(Ax, A, x);
+			x_cpu = HostVector<Scalar>(N);
+			x_cpu(all) = 1.0;
 			t = getUnixTime() - t_start;
 
-			std::cout << " minlin_full: " << t << "s, norm(Ax) = " << norm(Ax) << std::endl;
-			t_minlin_full += t;
+			std::cout << " cpu: " << t << "s, size = " << x_cpu.size()*sizeof(Scalar) << std::endl;
+			t_cpu += t;
 
 			/* if the dimension is small, then show also the content */
-			if(N <= 15) std::cout << "  " << Ax << std::endl;	
-		#endif
-
-		#if TEST_MINLIN
-			Ax(all) = default_value; /* clean previous results */
-
-			t_start = getUnixTime();
-			my_multiplication_minlin(Ax, x);
-			t = getUnixTime() - t_start;
-
-			std::cout << " minlin: " << t << "s, norm(Ax) = " << norm(Ax) << std::endl;
-			t_minlin += t;
-
-			if(N <= 15) std::cout << "  " << Ax << std::endl;	
-		#endif
-		
-		#if TEST_FOR
-			Ax(all) = default_value; /* clean previous results */
-
-			t_start = getUnixTime();
-			my_multiplication_for(Ax, x);
-			t = getUnixTime() - t_start;
-
-			std::cout << " for:    " << t << "s, norm(Ax) = " << norm(Ax) << std::endl;
-			t_for += t;
-
-			if(N <= 15) std::cout << "  " << Ax << std::endl;	
-		#endif
-		
-		#if TEST_OMP
-			Ax(all) = default_value; /* clean previous results */
-
-			t_start = getUnixTime();
-			my_multiplication_omp(Ax, x);
-			t = getUnixTime() - t_start;
-
-			std::cout << " omp:    " << t << "s, norm(Ax) = " << norm(Ax) << std::endl;
-			t_omp += t;
-
-			if (N <= 15) std::cout << "  " << Ax << std::endl;
+			if(N <= 15) std::cout << "x_cpu:" << "  " << x_cpu << std::endl;	
 
 		#endif
 
-		#if TEST_CUDA
-			Ax(all) = default_value; /* clean previous results */
+		#if TEST_GPU
 
 			t_start = getUnixTime();
-			my_multiplication_cuda(Ax, x);
+			x_gpu = DeviceVector<Scalar>(N);
+			x_gpu(all) = 1.0;
 			t = getUnixTime() - t_start;
 
-			std::cout << " cuda:   " << t << "s, norm(Ax) = " << norm(Ax) << std::endl;
-			t_cuda += t;
+			std::cout << " gpu: " << t << "s, size = " << x_gpu.size()*sizeof(Scalar) << std::endl;
+			t_gpu += t;
 
-			if(N <= 15) std::cout << "  " << Ax << std::endl;	
+			/* if the dimension is small, then show also the content */
+			if(N <= 15) std::cout << "x_gpu:" << "  " << x_gpu << std::endl;	
+			
 		#endif
 
 		std::cout << "-----------------------------------------------------------" << std::endl;
@@ -242,20 +131,11 @@ int main ( int argc, char *argv[] ) {
 	std::cout << "average times:" << std::endl;
 
 	/* compute and show average computing times */
-	#if TEST_MINLIN_FULL
-		std::cout << "minlin_full: " << t_minlin_full/(double)M << std::endl;
+	#if TEST_CPU
+		std::cout << "cpu: " << t_cpu/(double)M << std::endl;
 	#endif
-	#if TEST_MINLIN
-		std::cout << "minlin:      " << t_minlin/(double)M << std::endl;
-	#endif
-	#if TEST_FOR
-		std::cout << "for:         " << t_for/(double)M << std::endl;
-	#endif
-	#if TEST_OMP
-		std::cout << "omp:         " << t_omp/(double)M << std::endl;
-	#endif
-	#if TEST_CUDA
-		std::cout << "cuda:        " << t_cuda/(double)M << std::endl;
+	#if TEST_GPU
+		std::cout << "gpu: " << t_gpu/(double)M << std::endl;
 	#endif
 
 	
